4a3bb6bc55f49f10952261e532018abc05d354a7
Kon <kon.kim@outlook.com>
2016-08-04 10:37:05 -0400

    added templates folder

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,38 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.controller('ListCtrl', function($scope){
+
+  $scope.notes = [
+    {
+      title: 'First Note',
+      description: 'This is my first note'
+    },
+    {
+      title: 'Second Note',
+      description: 'This is my second note'
+    }
+  ];
+});
+
+app.config(function($stateProvider, $urlRouterProvider){
+  
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'templates/list.html'
+  });
+
+  $stateProvider.state('edit', {
+    url: '/list',
+    templateUrl: 'templates/list.html'
+  });
+
+  $urlRouterProvider.otherwise('/list');
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard

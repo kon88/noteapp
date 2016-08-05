c303f6b687ccc3ed21513537d5ba4c9613391f0c
Kon <kon.kim@outlook.com>
2016-08-04 21:12:42 -0400

    complete

--- www/js/app.js
+++ www/js/app.js
@@ -26,6 +26,7 @@ app.config(function($stateProvider, $urlRouterProvider){
 });
 
 app.controller('ListCtrl', function($scope, NoteStore){
+  $scope.reordering = false;
   $scope.notes = NoteStore.list();
   $scope.remove = function(noteId) {
     NoteStore.remove(noteId);
@@ -34,6 +35,11 @@ app.controller('ListCtrl', function($scope, NoteStore){
   $scope.move = function(note, fromIndex, toIndex) {
     NoteStore.move(note, fromIndex, toIndex);
   };
+
+  $scope.toggleReordering = function() {
+    $scope.reordering = !$scope.reordering;
+  }
+
 });
 
 app.controller('AddCtrl', function($scope, $state, NoteStore){
